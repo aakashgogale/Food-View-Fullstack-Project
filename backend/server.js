956a97a709659798a4bc6f85dec35b19ffd4486e
Aakash Gogale <aakashgogale123@gmail.com>
2026-02-24 14:32:36 +0530

    Initial commit: Food-View Fullstack Project

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectDB = require('./src/db/db');
 
+
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+dns.setDefaultResultOrder("ipv4first"); 
+
+
 connectDB();
 
 app.listen(3000, () => {
